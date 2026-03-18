67db8d479894dfe071413fb33997daa60aca1e30
AndreSoble <Andre_Soble@live.de>
2026-03-18 07:49:49 +0100

    feat: add GitHub Actions workflow for deployment and update base URL handling in components

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,7 @@ import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
 
 export default defineConfig({
+  site: 'https://andresoble.github.io',
+  base: '/cv-site',
   integrations: [tailwind({ applyBaseStyles: false }), react()],
 });
